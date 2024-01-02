dd9c000d476d27f263f8bf011046375391406678
waffleboi9000 <wafflebomb9000@gmail.com>
2024-01-02 12:20:22 -0500

    implemented pending, all, and completed tasks

--- PyTask.py
+++ PyTask.py
@@ -1,17 +1,28 @@
 tasks = {}
 running = True
-while running == True:
-    command = input("Enter a task to get started, \"pending\" to view a list of all pending tasks, \"completed\" for completed items.\n")
+while running is True:
+    command = input("Enter a task to get started, \"pending\" to view pending tasks, \"completed\" for completed items, or \"all\" for all items and their status. \n")
     if command == "pending":
         print('''all the pending tasks using dictionary stuff''')
+        print("Pending Tasks:")
+        for task, status in tasks.items():
+            if status == "pending": 
+                print(f"{task}")
+    # if a user enters "completed" print completed tasks.
     elif command == "completed":
-        print('''all the completed tasks using dictionary stuff''')
-        for items in tasks:
-            if value in tasks.values() == completed:
-                print(item)
-# if a user inputs something other than pending or completed, save it as a pending task.
+        print("Completed Tasks:") 
+        for items, status in tasks:
+            if status == "completed":
+                print({items}, {status})
+    # if a user enters EXIT, exit the program.
     elif command == "EXIT":
         running = False
+    # if a user enters all, print all tasks.
+    elif command == "all":
+        print("All Tasks:")  
+        for task, status in tasks.items():
+            print(f"[] {task}, {status}")
+    #if a user enters anything but the specified commands, save it as a pending task.
     else:
-        print('''saving the new task as pending''')
-# if a user enters EXIT, exit the program.
+        print("saving the new task as pending")
+        tasks[command] = 'pending'
