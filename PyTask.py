a26cf979b3beda2524c4f1db443302c5edc131d7
waffleboi9000 <wafflebomb9000@gmail.com>
2024-01-04 16:01:08 -0500

    stuck the whole thing in a while loop to make things better lol

--- PyTask.py
+++ PyTask.py
@@ -2,29 +2,30 @@ import pandas as pd
 import os.path
 running = True
 if os.path.isfile("Tasks.csv"):
-    # load tasks.csv as a dataframe into pandas
-    tasks = pd.read_csv("Tasks.csv")
-    # prompt user for a command
-    command = input("Enter a task to get started, \"pending\" to view pending tasks, \"completed\" for completed items, or \"all\" for all items and their status. \n")
-    if command == "pending":
-        # print out only pending tasks
-        pending_tasks = tasks[tasks["Status"] == "pending"]
-        print("Pending Tasks:")
-        print(pending_tasks.to_string())
-    elif command == "completed":
-        completed_tasks = tasks[tasks["Status"] == "completed"]
-        print("Completed Tasks:")
-        print(completed_tasks.to_string())
-    elif command == "all":
-        print("All Tasks:")
-        print(tasks.to_string())
-    elif command == "EXIT":
-        exit
-    elif tasks.loc[tasks["Task Name"] == command].empty:
-        print("That task isn't already in the list. Saving as a new pending task...")
-        # new_pending_task = pd.DataFrame(command, "pending")
-        tasks_list = open("Tasks.csv", "a")
-        tasks_list.write(command + ",pending")
+    while running:
+        # load tasks.csv as a dataframe into pandas
+        tasks = pd.read_csv("Tasks.csv")
+        # prompt user for a command
+        command = input("Enter a task to get started, \"pending\" to view pending tasks, \"completed\" for completed items, or \"all\" for all items and their status. \n")
+        if command == "pending":
+            # print out only pending tasks
+            pending_tasks = tasks[tasks["Status"] == "pending"]
+            print("Pending Tasks:")
+            print(pending_tasks.to_string())
+        elif command == "completed":
+            completed_tasks = tasks[tasks["Status"] == "completed"]
+            print("Completed Tasks:")
+            print(completed_tasks.to_string())
+        elif command == "all":
+            print("All Tasks:")
+            print(tasks.to_string())
+        elif command == "EXIT":
+            running = False
+        elif tasks.loc[tasks["Task Name"] == command].empty:
+            print("That task isn't already in the list. Saving as a new pending task...")
+            # new_pending_task = pd.DataFrame(command, "pending")
+            tasks_list = open("Tasks.csv", "a")
+            tasks_list.write(command + ",pending")
 
 # Error in case Tasks.csv isn't there.
 else:
