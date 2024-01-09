7de6c1bcc0c3474859bc17fd4d63cdd2b1226431
waffleboi9000 <wafflebomb9000@gmail.com>
2024-01-08 22:47:56 -0500

    fixed a leftover type bug and added some newlines where they were missing

--- PyTask.py
+++ PyTask.py
@@ -13,13 +13,13 @@ if os.path.isfile("Tasks.csv"):
             print("Pending Tasks:")
             print(pending_tasks.to_string())
             # ask for which task to modify, if any
-            modification = input("Enter a number of a task to change it to completed, or \"exit\" to leave this menu.")
+            modification = input("Enter a number of a task to change it to completed, or \"exit\" to leave this menu.\n")
             # if no modification, exit menu.
             if modification == "exit":
                 continue
             else:
                 # change given task to completed
-                tasks.at[modification, "Status"] = "completed"
+                tasks.at[int(modification), "Status"] = "completed"
                 tasks.to_csv("Tasks.csv", index=False)
                 # after modifying, print new pending tasks list.
                 print("All Tasks:")
@@ -29,7 +29,7 @@ if os.path.isfile("Tasks.csv"):
             print("Completed Tasks:")
             print(completed_tasks.to_string())
             # ask for which task to modify, if any
-            modification = input("Enter a number of a task to change it to pending, or \"exit\" to leave this menu.")
+            modification = input("Enter a number of a task to change it to pending, or \"exit\" to leave this menu.\n")
             # if no modification, exit menu.
             if modification == "exit":
                 continue
@@ -43,7 +43,7 @@ if os.path.isfile("Tasks.csv"):
         elif command == "all":
             print("All Tasks:")
             print(tasks.to_string())
-            modification = input("Enter the number of the task to modify, or \"exit\" to exit this menu.")
+            modification = input("Enter the number of the task to modify, or \"exit\" to exit this menu.\n")
             # exit the menu if user inputs exit
             if modification == "exit":
                 continue
@@ -59,14 +59,25 @@ if os.path.isfile("Tasks.csv"):
                 print(tasks.to_string())
         elif command == "exit":
             running = False
+        # if the task isn't in the list already, save it as a new pending task
         elif tasks.loc[tasks["Task Name"] == command].empty:
-            print("That task isn't already in the list. Saving as a new pending task...")
-            # new_pending_task = pd.DataFrame(command, "pending")
+            print("That task isn't already in the list. Saving as a new pending task...") 
             tasks_list = open("Tasks.csv", "a")
             tasks_list.write(command + ",pending\n")
             tasks_list.close()
             tasks = pd.read_csv("Tasks.csv")
-        # TODO: add a way to mark pending tasks as completed and completed as pending.
+        else:
+            new_status = input("What modification do you want to do? (completed, pending, delete)?")
+            # make this command an int, not a string.
+            tasks.at[command, "Status"] = new_status
+            tasks.to_csv("Tasks.csv", index=False)
+            # message about modification
+            print(f"Modified task {command}'s status to {new_status}.")
+            # after modifying, print new pending tasks list.
+            print("All Tasks:")
+            print(tasks.to_string())
+
+        # TODO: make a way to delete tasks
 # Error in case Tasks.csv isn't there.
 else:
     print("Tasks.csv not found! Attempting to make a new Tasks.csv.")
